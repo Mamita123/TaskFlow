0c02b7a35e9cc53722f5be55a46a8c753f7d0c2f
AnishPun <ANISH.PUN@metropolia.fi>
2024-11-28 15:36:25 +0200

    initial setup for TaskFLowproject

---
+++ TaskFlowCore/TaskFlowCore.h
@@ -0,0 +1,18 @@
+//
+//  TaskFlowCore.h
+//  TaskFlowCore
+//
+//  Created by Anish Pun on 28.11.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TaskFlowCore.
+FOUNDATION_EXPORT double TaskFlowCoreVersionNumber;
+
+//! Project version string for TaskFlowCore.
+FOUNDATION_EXPORT const unsigned char TaskFlowCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaskFlowCore/PublicHeader.h>
+
+
---
+++ TaskFlowUITests/TaskFlowCoreTests/TaskFlowCoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowCoreTests.swift
+//  TaskFlowCoreTests
+//
+//  Created by Anish Pun on 28.11.2024.
+//
+
+import Testing
+@testable import TaskFlowCore
+
+struct TaskFlowCoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
