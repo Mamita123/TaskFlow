c2f20a4d0c30aa0c39c3a95ba1b50e533a0338a6
AnishPun <ANISH.PUN@metropolia.fi>
2024-11-28 15:10:02 +0200

    Initial Commit

---
+++ TaskFlow/TaskFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowApp.swift
+//  TaskFlow
+//
+//  Created by Anish Pun on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskFlowTests/TaskFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowTests.swift
+//  TaskFlowTests
+//
+//  Created by Anish Pun on 28.11.2024.
+//
+
+import Testing
+@testable import TaskFlow
+
+struct TaskFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
